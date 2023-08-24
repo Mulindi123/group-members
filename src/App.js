6abf1282fca4d1a75a014b8ef7aa4f4caded9a6d
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-24 22:09:21 +0300

    mucsin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Mucsin from "./Mucsin";
 
 function App() {
   return (
     <div className="App">
-     
+      <Mucsin />
     </div>
   );
 }
---
+++ src/Mucsin.js
@@ -0,0 +1,4 @@
+function Mucsin() {
+  return <h1>Hi am mucsin</h1>;
+}
+export default Mucsin;
