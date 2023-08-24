6a0f0628f5403d6f2dbb65229a04c823a2522a25
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-24 23:36:10 +0300

    feat: added route to /joel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Joel from './Joel';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <nav>
+        <button><Link to='/joel'>Joel</Link></button>
+      </nav>
+     <Routes>
+      <Route path='/joel' element={<Joel/>}/>
+     </Routes>
     </div>
   );
 }
