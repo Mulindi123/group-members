211423659da3c51c572e4bf3962fe6db0b06ab88
Lehane-P <Lehane-P>
2023-08-25 00:20:22 +0300

    this is a new commit

---
+++ src/Lehane.js
@@ -0,0 +1,12 @@
+function Lehane () {
+    return (
+        <div>
+        <h2>My name is Lehane</h2>
+        <h2>I love swimming</h2>
+        <p>I am a Software Engineer</p>
+
+        </div>
+    )
+}
+
+export default Lehane;
