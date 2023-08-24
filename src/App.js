952c985e232d1095c0bc744195f2dc3bc6af4a21
Lehane <123290567+Lehane-P@users.noreply.github.com>
2023-08-24 22:54:04 +0300

    Lehane/lehanecomponents (#10)
    * add lehane component
    
    * added components
    
    * add changes
    
    ---------
    
    Co-authored-by: Lehane-P <Lehane-P>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Mucsin from "./Mucsin";
 
+import logo from './logo.svg';
+import './App.css';
+import Lehane from './Lehane';
 function App() {
   return (
     <div className="App">
-      <Mucsin />
+     <Lehane/>
     </div>
   );
 }
---
+++ src/Lehane.js
@@ -0,0 +1,10 @@
+function Lehane () {
+    return (
+        <div>
+        <h2>My name is Lehane</h2>
+        <h2>Software Engineer</h2>
+        </div>
+    )
+}
+
+export default Lehane;
