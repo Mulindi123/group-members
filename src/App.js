e0c4672b0eea81e5b94cf168a7bd166f1bbe0d3d
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-08-25 16:56:39 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Joel from './Joel';
 import Lehane from './Lehane';
 import Medrine from './Medrine';
+import Humphrey from './Humphrey';
 
 
 function App() {
@@ -13,11 +14,13 @@ function App() {
         <button><Link to='/joel'>Joel</Link></button>
         <button><Link to='/lehane'>Lehane</Link> </button>
         <button><Link to= "/medrine">Medrine</Link></button>
+        <button><Link to= "/humphrey">Humphrey</Link></button>
       </nav>
      <Routes>
       <Route path='/joel' element={<Joel/>}/>
       <Route path="/medrine" element={<Medrine/>} />
       <Route path='/lehane' element={<Lehane/>} />
+      <Route path='/humphrey' element={<Humphrey/>}/>
      </Routes>
     </div>
   );
--- src/Humphrey.js
+++ src/Humphrey.js
@@ -1,13 +1,16 @@
+// function Humphrey(){
+//     return<div>
+//         <h1>Hello,this is Humphrey's Component</h1>
+//     </div>
+// }
+
+// export default Humphrey
 function Humphrey(){
-    return<div>
-<<<<<<< HEAD
-        <h1>Hello,this is Humphrey's Component</h1>
-    </div>
+    return (
+        <div>
+            <h1>Hello, this is Humphrey's component</h1>
+        </div>
+    )
 }
 
 export default Humphrey
-=======
-        <h1>Hello,world  am Humphrey Njuguna</h1>
-    </div>
-}
->>>>>>> 4d8df025d5db8bca64178d7692c5f1ff4ce6f6a4
