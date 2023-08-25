e19adb79895c5e637d7104a28041faaa3ce68761
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-08-25 17:37:53 +0300

    changed

--- src/Humphrey.js
+++ src/Humphrey.js
@@ -9,6 +9,7 @@ function Humphrey(){
     return (
         <div>
             <h1>Hello, this is Humphrey's component</h1>
+            <img src="https://images.pexels.com/photos/115655/pexels-photo-115655.jpeg?auto=compress&cs=tinysrgb&w=600" alt="humph"></img>
         </div>
     )
 }
